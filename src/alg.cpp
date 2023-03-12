61de2e94bb6438453f018f590ffc061507b91162
IrinaRyabchikova <116703957+IrinaRyabchikova@users.noreply.github.com>
2023-03-12 16:37:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
 
+int lower_bound(int *arr, int size, int value) {
+  int left = 0, right = size;
+  if (arr[0] >= value) {
+    return 0;
+  }
+  while (right - left > 1) {
+    int mid = (right + left) / 2;
+    if (arr[mid] >= value) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+  return right;
+}
+
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  return lower_bound(arr, size, value + 1) - lower_bound(arr, size, value);
 }
